605f95ec8f4e81d1be64b2399627682d46290652
ShinjiK1 <92060952+ShinjiK1@users.noreply.github.com>
2023-10-18 12:20:31 -0400

    Added html and js files

---
+++ game.js
@@ -0,0 +1,8 @@
+// Halloween game by Shinji, Kevin and Selena
+
+
+
+var gameState = 0; //Different value for each mode, we will run the code for the mode that corresponds to each value.
+var c = document.getElementById("playground");
+var ctx = c.getContext("2d");
+var startButton = document.getElementById("buttonStart");
