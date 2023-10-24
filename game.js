085de7973494e9d9de61fe970f2f3d43cf480af4
KevinLi0711 <108951684+Luominai@users.noreply.github.com>
2023-10-24 10:48:34 -0400

    put draggable div into game.html

--- game.js
+++ game.js
@@ -29,3 +29,30 @@ fetch(request)
       console.error(error);
    });
 
+
+// Code for draggable divs ===========================================
+var draggables = document.querySelectorAll("div.draggable")
+draggables.forEach(element => {
+    element.onmousedown = (mouseDown => {
+        // Get initial coords of mouse
+        let initialMouseX = mouseDown.clientX
+        let initialMouseY = mouseDown.clientY
+        // Get initial coords of element
+        let initialElementX = element.offsetLeft
+        let initialElementY = element.offsetTop
+
+        document.onmousemove = (mouseMove => {
+            // Get difference between current mouse coords and initial
+            let offsetX = mouseMove.clientX - initialMouseX
+            let offsetY = mouseMove.clientY - initialMouseY
+
+            // Adjust the position of the element
+            element.style.left = parseInt(initialElementX) + parseInt(offsetX) + "px"
+            element.style.top = parseInt(initialElementY) + parseInt(offsetY) + "px"
+        })
+    })
+})
+document.onmouseup = (mouseEvent => {
+    document.onmousemove = null // Remove existing onmousemove actions
+})
+// End code for draggable divs =======================================
